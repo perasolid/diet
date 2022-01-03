969c28fb85b8159a47953eda15e89806c811a6eb
krjovan <jovankron@gmail.com>
2022-01-03 14:17:33 +0100

    Add password change function by logged-in user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var crypto = require('crypto');
 
 module.exports.getAll = function(req, res) {
   User.find(function(err, users){
@@ -10,6 +11,37 @@ module.exports.getAll = function(req, res) {
             res.json(users);
         }
     });
+};
+
+module.exports.updateUser = function(req, res) {
+	if(req.body.newPassword !== '') {
+		User.findOneAndUpdate({_id: req.params.id},{
+			$set:{
+			  hash: crypto.pbkdf2Sync(req.body.newPassword, req.body.salt, 1000, 64, 'sha512').toString('hex')
+			}
+		},
+		function(err,result){
+			if(err) {
+				console.log(err);
+			}
+			else {
+				console.log(result);
+			}
+		});
+	}
+	User.findOneAndUpdate({_id: req.params.id},{
+		$set:{
+		  email:req.body.email
+		}
+	},
+	function(err,result){
+		if(err) {
+			res.json(err);
+		}
+		else {
+			res.json(result);
+		}	
+	});
 };
 
 module.exports.deleteUser = function(req, res) {
--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,7 @@ router.post('/login', ctrlAuth.login);
 
 //retreving data from database
 router.get('/all', ctrlUser.getAll);
+router.put('/update/:id', ctrlUser.updateUser);
 router.delete('/delete/:id', ctrlUser.deleteUser);
 
 module.exports = router;
