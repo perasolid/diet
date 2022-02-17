653a6b7473dfaca19393dcb27011bd4d4812a501
krjovan <jovankron@gmail.com>
2022-02-17 12:28:20 +0100

    Changed nutrition model

--- models/nutrition.js
+++ models/nutrition.js
@@ -6,298 +6,138 @@ var nutritionSchema = new mongoose.Schema({
 		required: true,
 		unique: true
 	},
-	calories: {
+	calories: { 
 		type: Number,
 		required: true
 	},
-	total_fat_g: {
+	carbohydrate_g: { 
 		type: Number,
 		required: true
 	},
-	saturated_fat_g: {
+	fiber_g: { 
 		type: Number,
 		required: true
 	},
-	cholesterol_mg: {
+	protein_g: { 
 		type: Number,
 		required: true
 	},
-	sodium_mg: {
+	total_fat_g: { 
 		type: Number,
 		required: true
 	},
-	choline_mg: {
+	saturated_fat_g: { 
 		type: Number,
 		required: true
 	},
-	folate_mcg: {
+	fatty_acids_total_trans_g: { 
 		type: Number,
 		required: true
 	},
-	folic_acid_mcg: {
+	cholesterol_mg: { 
 		type: Number,
 		required: true
 	},
-	niacin_mg: {
+	sugars_g: { 
 		type: Number,
 		required: true
 	},
-	pantothenic_acid_mg: {
+	water_g: { 
 		type: Number,
 		required: true
 	},
-	riboflavin_mg: {
+	vitamin_a_rae_mcg: { 
 		type: Number,
 		required: true
 	},
-	thiamin_mg: {
+	thiamin_mg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_a_IU: {
+	riboflavin_mg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_a_rae_mcg: {
+	niacin_mg: { 
 		type: Number,
 		required: true
 	},
-	carotene_alpha_mcg: {
+	pantothenic_acid_mg: { 
 		type: Number,
 		required: true
 	},
-	carotene_beta_mcg: {
+	vitamin_b6_mg: { 
 		type: Number,
 		required: true
 	},
-	cryptoxanthin_beta_mcg: {
+	folate_mcg: { 
 		type: Number,
 		required: true
 	},
-	lutein_zeaxanthin_mcg: {
+	vitamin_b12_mcg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_b12_mcg: {
+	choline_mg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_b6_mg: {
+	vitamin_c_mg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_c_mg: {
+	vitamin_d_IU: { 
 		type: Number,
 		required: true
 	},
-	vitamin_d_IU: {
+	vitamin_e_mg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_e_mg: {
+	vitamin_k_mcg: { 
 		type: Number,
 		required: true
 	},
-	tocopherol_alpha_mg: {
+	calcium_mg: { 
 		type: Number,
 		required: true
 	},
-	vitamin_k_mcg: {
+	copper_mg: { 
 		type: Number,
 		required: true
 	},
-	calcium_mg: {
+	irom_mg: { 
 		type: Number,
 		required: true
 	},
-	copper_mg: {
+	magnesium_mg: { 
 		type: Number,
 		required: true
 	},
-	irom_mg: {
+	manganese_mg: { 
 		type: Number,
 		required: true
 	},
-	magnesium_mg: {
+	phosphorous_mg: { 
 		type: Number,
 		required: true
 	},
-	manganese_mg: {
+	potassium_mg: { 
 		type: Number,
 		required: true
 	},
-	phosphorous_mg: {
+	selenium_mcg: { 
 		type: Number,
 		required: true
 	},
-	potassium_mg: {
+	sodium_mg: { 
 		type: Number,
 		required: true
 	},
-	selenium_mcg: {
+	zink_mg: { 
 		type: Number,
 		required: true
-	},
-	zink_mg: {
-		type: Number,
-		required: true
-	},
-	protein_g: {
-		type: Number,
-		required: true
-	},
-	alanine_g: {
-		type: Number,
-		required: true
-	},
-	arginine_g: {
-		type: Number,
-		required: true
-	},
-	aspartic_acid_g: {
-		type: Number,
-		required: true
-	},
-	cystine_g: {
-		type: Number,
-		required: true
-	},
-	glutamic_acid_g: {
-		type: Number,
-		required: true
-	},
-	glycine_g: {
-		type: Number,
-		required: true
-	},
-	histidine_g: {
-		type: Number,
-		required: true
-	},
-	hydroxyproline_g: {
-		type: Number,
-		required: true
-	},
-	isoleucine_g: {
-		type: Number,
-		required: true
-	},
-	leucine_g: {
-		type: Number,
-		required: true
-	},
-	lysine_g: {
-		type: Number,
-		required: true
-	},
-	methionine_g: {
-		type: Number,
-		required: true
-	},
-	phenylalanine_g: {
-		type: Number,
-		required: true
-	},
-	proline_g: {
-		type: Number,
-		required: true
-	},
-	serine_g: {
-		type: Number,
-		required: true
-	},
-	threonine_g: {
-		type: Number,
-		required: true
-	},
-	tryptophan_g: {
-		type: Number,
-		required: true
-	},
-	tyrosine_g: {
-		type: Number,
-		required: true
-	},
-	valine_g: {
-		type: Number,
-		required: true
-	},
-	carbohydrate_g: {
-		type: Number,
-		required: true
-	},
-	fiber_g: {
-		type: Number,
-		required: true
-	},
-	sugars_g: {
-		type: Number,
-		required: true
-	},
-	fructose_g: {
-		type: Number,
-		required: true
-	},
-	galactose_g: {
-		type: Number,
-		required: true
-	},
-	glucose_g: {
-		type: Number,
-		required: true
-	},
-	lactose_g: {
-		type: Number,
-		required: true
-	},
-	maltose_g: {
-		type: Number,
-		required: true
-	},
-	sucrose_g: {
-		type: Number,
-		required: true
-	},
-	fat_g: {
-		type: Number,
-		required: true
-	},
-	saturated_fatty_acids_g: {
-		type: Number,
-		required: true
-	},
-	monounsaturated_fatty_acids_g: {
-		type: Number,
-		required: true
-	},
-	polyunsaturated_fatty_acids_g: {
-		type: Number,
-		required: true
-	},
-	fatty_acids_total_trans_g: {
-		type: Number,
-		required: true
-	},
-	alcohol_g: {
-		type: Number,
-		required: true
-	},
-	ash_g: {
-		type: Number,
-		required: true
-	},
-	caffeine_mg: {
-		type: Number,
-		required: true
-	},
-	theobromine_mg: {
-		type: Number,
-		required: true
-	},
-	water_g: {
-		type: Number,
-		required: true
-	},
+	}
 });
 
 const Nutrition = module.exports = mongoose.model('Nutrition', nutritionSchema);
