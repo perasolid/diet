89eba58f9df53f302354f945c978c141247a4468
krjovan <jovankron@gmail.com>
2022-03-12 12:49:39 +0100

    Removed require mongoose in passport

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 passport.use(new LocalStrategy({
