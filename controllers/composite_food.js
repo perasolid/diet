96404db479b087fe971540a3f44a1f05736166d5
krjovan <jovankron@gmail.com>
2022-04-08 23:32:39 +0200

    Added controller for adding composite food and ingredients

---
+++ controllers/composite_food.js
@@ -0,0 +1,21 @@
+const CompositeFood = mongoose.model("CompositeFood");
+const Nutrition = mongoose.model("Nutrition");
+
+module.exports.addCompositeFood = async function(req, res) {
+    let nutrition = new Nutrition(req.body.calculatedNutrition);
+	nutrition.save()
+    .then(nutrition => {
+        console.log(nutrition._id);
+        let ingredientsToBeInserted = req.body.ingredients.map(ingredient => ({ ...ingredient, composite_food_id: nutrition._id }));
+        CompositeFood.insertMany(ingredientsToBeInserted)
+        .then(() => res.status(200).json({"message":"Created composite food!"}))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json({"message":"Something went wrong while creating ingredients"});
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        return res.status(500).json({"message":"Something went wrong while creating composite food"});
+    });
+}
