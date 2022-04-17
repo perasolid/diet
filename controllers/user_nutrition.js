2b279e944d9eb9dbd1d3397c8a17780ba2680f99
krjovan <jovankron@gmail.com>
2022-04-17 13:05:02 +0200

    Refactored users controller

--- controllers/user_nutrition.js
+++ controllers/user_nutrition.js
@@ -3,12 +3,12 @@ const isValidObjectId = require('../config/is_object_id');
 
 module.exports.getAll = (req, res) => {
 	User_nutrition.find()
-	.sort({ _id: -1 })
-	.exec((err, doc) => {
-        if(err)
-            return res.status(500).json(err);
-        res.status(200).json(doc);
-    });
+		.sort({ _id: -1 })
+		.exec((err, doc) => {
+			if (err)
+				return res.status(500).json(err);
+			res.status(200).json(doc);
+		});
 };
 
 module.exports.getUser_nutrition = (req, res) => {
@@ -17,7 +17,7 @@ module.exports.getUser_nutrition = (req, res) => {
 	let date = new Date(req.query.date_of_consumption);
 	const endOfDay = new Date(date.setDate(date.getDate() + 1));
 	User_nutrition.aggregate([
-		{   
+		{
 			$match: {
 				user_id: id,
 				date_of_consumption: { $gte: startOfDay, $lt: endOfDay }
@@ -32,41 +32,41 @@ module.exports.getUser_nutrition = (req, res) => {
 				as: "nutrition"
 			}
 		},
-		{ $unwind : "$nutrition" }
+		{ $unwind: "$nutrition" }
 	]).exec((err, list) => {
-        if (err)
+		if (err)
 			throw err;
 		res.status(200).json(list);
-    }); 
+	});
 };
 
 module.exports.addUser_nutrition = (req, res) => {
 	const user_nutrition = new User_nutrition(req.body);
 	user_nutrition.save((err) => {
-        if(err) 
-            res.status(400).json(err);
-        else
-            res.status(200).json({message: 'Successfully added food to diet!'});
-    });
+		if (err)
+			res.status(400).json(err);
+		else
+			res.status(200).json({ message: 'Successfully added food to diet!' });
+	});
 };
 
 module.exports.updateUser_nutrition = (req, res) => {
-	if(!isValidObjectId(req.params.id))
-        return res.status(400).json({message: "Invalid user-nutrition id format"})
+	if (!isValidObjectId(req.params.id))
+		return res.status(400).json({ message: "Invalid user-nutrition id format" })
 	User_nutrition.update({ _id: mongoose.Types.ObjectId(req.params.id) }, req.body)
-    .then(() => {
-        res.status(200).json();
-    })
-    .catch((err) => {
-        res.status(404).send(err);
-    });
+		.then(() => {
+			res.status(200).json();
+		})
+		.catch((err) => {
+			res.status(404).send(err);
+		});
 };
 
 module.exports.deleteUser_nutrition = (req, res) => {
-	if(!isValidObjectId(req.params.id))
-		return res.status(400).json({message: "Invalid user-nutrition id format"})
-	User_nutrition.remove({_id: req.params.id}, (err, result) => {
-		if(err)
+	if (!isValidObjectId(req.params.id))
+		return res.status(400).json({ message: "Invalid user-nutrition id format" })
+	User_nutrition.remove({ _id: req.params.id }, (err, result) => {
+		if (err)
 			res.json(err);
 		else
 			res.json(result);
--- controllers/users.js
+++ controllers/users.js
@@ -1,140 +1,140 @@
-var User = mongoose.model('User');
-var crypto = require('crypto');
+const User = mongoose.model('User');
+const crypto = require('crypto');
 const email_config = require('../config/email');
-const html_templates = require('../defaults/html_templates'); 
+const html_templates = require('../defaults/html_templates');
 
-module.exports.getAll = function(req, res) {
-  const regex = new RegExp(req.query.search, 'i')
-	User.find({name: {$regex: regex}})
-	.sort({ _id: -1 })
-	.exec(function (err, doc) {
-			if(err)
+module.exports.getAll = (req, res) => {
+	const regex = new RegExp(req.query.search, 'i');
+	User.find({ name: { $regex: regex } })
+		.sort({ _id: -1 })
+		.exec((err, doc) => {
+			if (err)
 				return res.status(500).json(err);
 			res.status(200).json(doc);
-    });
+		});
 };
 
-module.exports.addUser = function(req, res) {
-	if(!req.body.name || !req.body.email || !req.body.password)
-    return res.status(400).json({"message": "All fields required"});
+module.exports.addUser = (req, res) => {
+	if (!req.body.name || !req.body.email || !req.body.password)
+		return res.status(400).json({ "message": "All fields required" });
 
-  User.findOne({email: req.body.email}, function(err, user) {
-    if(err)
-      return console.log(err);
-    if(user)
-      return res.status(400).json({"message": "Email taken"});
-    else {
-      var user = new User();
-      user.name = req.body.name;
-      user.email = req.body.email;
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (err)
+			return console.log(err);
+		if (user)
+			return res.status(400).json({ "message": "Email taken" });
+		else {
+			let user = new User();
+			user.name = req.body.name;
+			user.email = req.body.email;
 			user.setPassword(req.body.password);
 			user.save().then(() => {
-				res.status(200).json({"message" : "User created successfully"});
+				res.status(200).json({ "message": "User created successfully" });
 			});
-    }
-  });
+		}
+	});
 };
 
-module.exports.updateUser = function(req, res) {
+module.exports.updateUser = (req, res) => {
 	if (req.body.newPassword !== '')
-		req.body.hash = crypto.pbkdf2Sync(req.body.newPassword, req.body.salt, 1000, 64, 'sha512').toString('hex')
-	User.findOneAndUpdate({_id: req.params.id},{
+		req.body.hash = crypto.pbkdf2Sync(req.body.newPassword, req.body.salt, 1000, 64, 'sha512').toString('hex');
+	User.findOneAndUpdate({ _id: req.params.id }, {
 		$set: req.body
 	},
-	function(err, result){
-		if(err)
-			res.json(err);
-		else
-			res.json(result);
-	});
+		(err, result) => {
+			if (err)
+				res.json(err);
+			else
+				res.json(result);
+		});
 };
 
-module.exports.updateUserByAdmin = function(req, res) {
-	User.findOneAndUpdate({_id: req.params.id},{
-		$set:{
-		  name:req.body.name,
-		  email:req.body.email,
-		  role:req.body.role
+module.exports.updateUserByAdmin = (req, res) => {
+	User.findOneAndUpdate({ _id: req.params.id }, {
+		$set: {
+			name: req.body.name,
+			email: req.body.email,
+			role: req.body.role
 		}
 	},
-	function(err, result){
-		if(err)
+		(err, result) => {
+			if (err)
+				res.json(err);
+			else
+				res.json(result);
+		});
+};
+
+module.exports.getHash = (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify({ hash: crypto.pbkdf2Sync(req.body.currentPassword, req.body.salt, 1000, 64, 'sha512').toString('hex') }));
+};
+
+module.exports.deleteUser = (req, res) => {
+	User.remove({ _id: req.params.id }, (err, result) => {
+		if (err)
 			res.json(err);
 		else
 			res.json(result);
 	});
-};
-
-module.exports.getHash = function(req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({ hash: crypto.pbkdf2Sync(req.body.currentPassword, req.body.salt, 1000, 64, 'sha512').toString('hex')  }));
-};
-
-module.exports.deleteUser = function(req, res) {
-  User.remove({_id: req.params.id}, function(err, result) {
-        if(err)
-          res.json(err);
-        else
-          res.json(result);
-  });
 };
 
 function makeid(length) {
-	var result = '';
-	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
-	var charactersLength = characters.length;
-	for ( var i = 0; i < length; i++ )
+	let result = '';
+	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+	let charactersLength = characters.length;
+	for (let i = 0; i < length; i++)
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
 	return result;
 }
 
-module.exports.resetPassword = function(req, res) {
-	var password = makeid(10);
-	User.findOneAndUpdate({_id: req.body._id},{
-			$set:{
-			  hash: crypto.pbkdf2Sync(password, req.body.salt, 1000, 64, 'sha512').toString('hex')
-			}
+module.exports.resetPassword = (req, res) => {
+	const password = makeid(10);
+	User.findOneAndUpdate({ _id: req.body._id }, {
+		$set: {
+			hash: crypto.pbkdf2Sync(password, req.body.salt, 1000, 64, 'sha512').toString('hex')
+		}
 	})
-	.then(() => {
-		var mailOptions = {
-			from: process.env.EMAIL,
-			to: req.body.email,
-			subject: 'Reset password',
-			html: html_templates.reset_password.replace(/####/g, password)
-		};
-		email_config.transport.sendMail(mailOptions, function(err, info) {
-			if (err)
-				res.status(503).json({"message" : err.message});
-			else
-				res.status(200).json({"message" : "Password reset successfully!"});
-		});
-	})
-	.catch((err) => {
-		res.status(503).json({"message" : err.message});
-	})
+		.then(() => {
+			let mailOptions = {
+				from: process.env.EMAIL,
+				to: req.body.email,
+				subject: 'Reset password',
+				html: html_templates.reset_password.replace(/####/g, password)
+			};
+			email_config.transport.sendMail(mailOptions, (err) => {
+				if (err)
+					res.status(503).json({ "message": err.message });
+				else
+					res.status(200).json({ "message": "Password reset successfully!" });
+			});
+		})
+		.catch((err) => {
+			res.status(503).json({ "message": err.message });
+		})
 }
 
-module.exports.getUsersByPagination = function(req, res) {
+module.exports.getUsersByPagination = (req, res) => {
 	const pageOptions = {
 		page: parseInt(req.query.page, 10) || 0,
 		limit: parseInt(req.query.limit, 10) || 10
 	}
 	User.find()
-	.sort({ _id: -1 })
-    .skip(pageOptions.page * pageOptions.limit)
-    .limit(pageOptions.limit)
-    .exec(function (err, doc) {
-			if(err)
+		.sort({ _id: -1 })
+		.skip(pageOptions.page * pageOptions.limit)
+		.limit(pageOptions.limit)
+		.exec((err, doc) => {
+			if (err)
 				return res.status(500).json(err);
-      res.status(200).json(doc);
-    });
+			res.status(200).json(doc);
+		});
 }
 
-module.exports.numberOfUsers = function(req, res) {
+module.exports.numberOfUsers = (req, res) => {
 	User.countDocuments()
-    .exec(function (err, doc) {
-			if(err)
+		.exec((err, doc) => {
+			if (err)
 				return res.status(500).json(err);
-			res.status(200).json({"numberOfUsers":doc});
-    });
+			res.status(200).json({ "numberOfUsers": doc });
+		});
 }
