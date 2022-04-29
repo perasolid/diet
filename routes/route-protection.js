fa9cc7df74b6dd6620ba1dd5129ad9fa93e75223
krjovan <jovankron@gmail.com>
2022-04-29 16:02:14 +0200

    Excluded sign-in, register and home frontend routes from being auth blocked

--- routes/route-protection.js
+++ routes/route-protection.js
@@ -1,13 +1,17 @@
 const jwt = require('express-jwt');
 const jwtPermissions = require('express-jwt-permissions');
 
+let unprotectedRoutes = ['/login', '/register', '/resend-verification-email',
+    '/verifyAccount', '/verifyRecaptcha']
+    .map(relativeRoute => '/users' + relativeRoute);      
+unprotectedRoutes.push('/sign-in','/register', '/home');
+
 module.exports.checkUserAuthentication = jwt({
     secret: process.env.SECRET,
     userProperty: 'payload',
     algorithms: ['HS256']
 }).unless({
-    path: ['/login', '/register', '/resend-verification-email',
-        '/verifyAccount', '/verifyRecaptcha'].map(relativeRoute => '/users' + relativeRoute)
+    path: unprotectedRoutes
 });
 
 module.exports.adminGuard = jwtPermissions({
